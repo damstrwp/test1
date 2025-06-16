215ed2cee492b8704f894b988aa062ad681776f9
Dasha <d.asha_popova2008@mail.ru>
2025-06-16 17:49:36 +0500

    edit callbacks

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -10,26 +10,61 @@ from states import Form
 callback_router = Router()
 
 
-@callback_router.callback_query(F.data == "help")
-async def handle_helpbutton(callback: aiogram.types.CallbackQuery):
-    await callback.answer("Уже спешу помочь!")
-    help_message = (f"<b>Я всегда готов помочь!</b> \n"
-                    f"<i>Управление:</i>\n"
-                    f"/start - начало работы \n"
-                    f"/help - навигация \n"
-                    f"/menu  - меню \n"
-                    f"/about - расскажет о чем этот бот \n"
-                    f"<i>Поиск фильма:</i>\n"
-                    f"/films - найти фильмы по критериям \n"
-                    f"/film - поиск определенного фильма по названию\n"
-                    f"/actor - фильмы с определенным актером ")
-    await callback.message.answer(text=help_message, parse_mode="HTML")
+@callback_router.callback_query(F.data == "start")#после нажатия кнопки "Начнем!"
+async def handle_start(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await callback.message.edit_caption(
+        caption=f"Привет,{callback.from_user.username}! Это бот для поиска интересных фильмов по вашим интересам.",
+        reply_markup=None)
+    await callback.message.answer(f"С чего начнем?", reply_markup=menu_keyboard)
 
 
-@callback_router.callback_query(F.data == "about")
-async def handle_aboutbutton(callback: aiogram.types.CallbackQuery):
-    await callback.answer("О нас")
-    await callback.message.answer(f"Этот бот ищет фильмы по вашим критериям и выводит их рейтинг.")
+@callback_router.callback_query(F.data == "menu")#нажатие кнопки "Управление"
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await callback.message.edit_text("Выберите функцию", reply_markup=uprav_keyboard)
+
+
+@callback_router.callback_query(F.data == "menu_help")
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await handle_help(callback.message)  # ссылаюсь на функцию из commands
+
+
+@callback_router.callback_query(F.data == "menu_about")  # при нажатии кнопки "о боте",выводится информация о нем
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await handle_about(callback.message)
+
+
+@callback_router.callback_query(F.data == "menu_menu")
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await handle_menu(callback.message)
+
+
+@callback_router.callback_query(F.data == "menu2")
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await callback.message.edit_text("Выберите функцию", reply_markup=poisk_keyboard)
+
+
+@callback_router.callback_query(F.data == "menu2_films")
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await handle_films(callback.message)
+
+
+@callback_router.callback_query(F.data == "menu2_poisk")
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await handle_film(callback.message)
+
+
+@callback_router.callback_query(F.data == "menu2_randfilm")
+async def handle_message(callback: aiogram.types.CallbackQuery):
+    await callback.answer()
+    await handle_randfilms(callback.message)
 
 
 @callback_router.callback_query(
@@ -68,67 +103,11 @@ async def handle_arrow(callback: aiogram.types.CallbackQuery):
                                      reply_markup=film_keyboard)
 
 
-@callback_router.callback_query(F.data == "start")
-async def handle_start(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await callback.message.edit_caption(caption=f"Привет,{callback.from_user.username}! Это бот для поиска интересных фильмов по вашим интересам.",
-                                        reply_markup=None)
-    await callback.message.answer(f"С чего начнем?", reply_markup=menu_keyboard)
-
-
 @callback_router.callback_query(F.data == "continue")
 async def handle_start(callback: aiogram.types.CallbackQuery):
     await callback.answer()
     await callback.message.edit_reply_markup(reply_markup=None)
     await callback.message.answer(f"С чего начнем?", reply_markup=menu_keyboard)
-
-
-@callback_router.callback_query(F.data == "menu")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await callback.message.edit_text("Выберите функцию", reply_markup=uprav_keyboard)
-
-
-@callback_router.callback_query(F.data == "menu2")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await callback.message.edit_text("Выберите функцию", reply_markup=poisk_keyboard)
-
-
-@callback_router.callback_query(F.data == "menu2_films")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await handle_films(callback.message)
-
-
-@callback_router.callback_query(F.data == "menu2_poisk")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await handle_film(callback.message)
-
-
-@callback_router.callback_query(F.data == "menu2_randfilm")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await handle_randfilms(callback.message)
-
-
-@callback_router.callback_query(F.data == "menu_help")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await handle_help(callback.message)
-
-
-@callback_router.callback_query(F.data == "menu_menu")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await handle_menu(callback.message)
-
-
-@callback_router.callback_query(F.data == "menu_about")
-async def handle_message(callback: aiogram.types.CallbackQuery):
-    await callback.answer()
-    await handle_about(callback.message)
 
 
 @callback_router.callback_query(F.data == "back")
